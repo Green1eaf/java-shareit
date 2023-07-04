a89abaa16ec9b69d909bb603ebbbdc6407780f2b
aleckid77@gmail.com <MyGitHub1>
2023-07-04 18:09:59 +0300

    feat: model of User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,27 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotNull
+    private String email;
 }
