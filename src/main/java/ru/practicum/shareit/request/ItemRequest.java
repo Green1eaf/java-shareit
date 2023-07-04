3887f73657f833e0e70204935efe66070d5d3ce0
aleckid77@gmail.com <MyGitHub1>
2023-07-04 18:12:09 +0300

    feat: added ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemRequest {
+
+    private Long id;
+
+    private String description;
+
+    private User requestor;
+
+    private LocalDateTime created;
 }
