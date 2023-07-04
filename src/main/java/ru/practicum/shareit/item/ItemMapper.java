21042dfa60bfd18849d30ac635cdc762349b7209
aleckid77@gmail.com <MyGitHub1>
2023-07-04 18:11:00 +0300

    feat: added ItemDto and ItemMapper

---
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -0,0 +1,16 @@
+package ru.practicum.shareit.item;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+public class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .request(item.getRequest() != null ? item.getRequest() : null)
+                .build();
+    }
+}
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,25 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.request.ItemRequest;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ItemDto {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Boolean available;
+
+    private ItemRequest request;
 }
