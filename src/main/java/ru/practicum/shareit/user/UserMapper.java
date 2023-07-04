fd6f16f52071acb14999d2346860388999ba42da
aleckid77@gmail.com <MyGitHub1>
2023-07-04 18:10:23 +0300

    feat: added UserDto and UserMapper

---
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.user;
+
+import ru.practicum.shareit.user.dto.UserDto;
+
+public class UserMapper {
+    public static UserDto toUserDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+}
