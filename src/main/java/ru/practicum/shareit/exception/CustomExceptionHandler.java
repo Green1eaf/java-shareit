07c4053d347ac1abf2b3a87b1db351d7b41d22b7
aleckid77@gmail.com <MyGitHub1>
2023-07-05 20:10:45 +0300

    feat: added AlreadyExistsException and CustomExceptionHandler

---
+++ src/main/java/ru/practicum/shareit/exception/CustomExceptionHandler.java
@@ -0,0 +1,48 @@
+package ru.practicum.shareit.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseError conflictHandle(AlreadyExistsException exception) {
+        log.error(exception.getMessage());
+        return new ResponseError(exception.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseError notFoundHandle(NotExistException exception) {
+        log.error(exception.getMessage());
+        return new ResponseError(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseError handle(RuntimeException exception) {
+        log.error(exception.getMessage());
+        return new ResponseError(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @Getter
+    @RequiredArgsConstructor
+    private static class ResponseError {
+        private final String message;
+        private final HttpStatus status;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        private final LocalDateTime time = LocalDateTime.now();
+    }
+}
