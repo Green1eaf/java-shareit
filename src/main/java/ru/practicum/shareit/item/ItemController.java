76056c725e3bf431f3473960b0c0419ea6945e74
aleckid77@gmail.com <MyGitHub1>
2023-07-08 23:10:29 +0300

    fix: logging for users and items controllers

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDto;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/items")
+@Slf4j
 public class ItemController {
 
     private final ItemService itemService;
@@ -18,27 +20,32 @@ public class ItemController {
 
     @PostMapping
     public ItemDto create(@RequestBody @Valid ItemDto itemDto, @RequestHeader("X-Sharer-User-Id") long userId) {
+        log.info("POST method: create item");
         return itemService.create(itemDto, userId);
     }
 
     @PatchMapping("/{itemId}")
     public ItemDto update(@RequestBody ItemDto itemDto, @PathVariable long itemId,
                           @RequestHeader("X-Sharer-User-Id") long userId) {
+        log.info("PATCH method: update item with id{}", itemId);
         return itemService.update(itemDto, itemId, userId);
     }
 
     @GetMapping("/{itemId}")
     public ItemDto findById(@PathVariable long itemId) {
+        log.info("GET method: get item by id={}", itemId);
         return itemService.findById(itemId);
     }
 
     @GetMapping
     public List<ItemDto> findAllByUserId(@RequestHeader("X-Sharer-User-Id") long userId) {
+        log.info("GET method: find all items for user with id={}", userId);
         return itemService.findAllByUserId(userId);
     }
 
     @GetMapping("/search")
     public List<ItemDto> findByText(@RequestParam(required = false, name = "text") String text) {
+        log.info("GET method: find item by text={}", text);
         return itemService.searchByText(text);
     }
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.user;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 
@@ -8,36 +10,39 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @PostMapping
     public UserDto create(@RequestBody @Valid UserDto userDto) {
+        log.info("POST method: create user");
         return userService.create(userDto);
     }
 
     @GetMapping("/{id}")
     public UserDto findById(@PathVariable long id) {
+        log.info("GET method: find user with id={}", id);
         return userService.findById(id);
     }
 
     @PatchMapping("/{userId}")
     public UserDto update(@RequestBody UserDto userDto, @PathVariable long userId) {
+        log.info("PATCH method: update user with id={}", userId);
         return userService.update(userDto, userId);
     }
 
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable long id) {
+        log.info("DELETE method: delete user with id={}", id);
         userService.deleteById(id);
     }
 
     @GetMapping
     public List<UserDto> findAll() {
+        log.info("GET method: find all users");
         return userService.findAll();
     }
 }
