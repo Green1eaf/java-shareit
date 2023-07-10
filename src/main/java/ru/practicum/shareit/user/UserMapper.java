eee351e291c566992eea4e12268c716dfc0e0364
aleckid77@gmail.com <MyGitHub1>
2023-07-10 09:10:10 +0300

    fix: mappers fixed (static)

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -5,7 +5,7 @@ import ru.practicum.shareit.user.dto.UserDto;
 
 @UtilityClass
 public class UserMapper {
-    public UserDto toUserDto(User user) {
+    public static UserDto toUserDto(User user) {
         return UserDto.builder()
                 .id(user.getId())
                 .name(user.getName())
@@ -13,7 +13,7 @@ public class UserMapper {
                 .build();
     }
 
-    public User toUser(UserDto userDto) {
+    public static User toUser(UserDto userDto) {
         return User.builder()
                 .id(userDto.getId())
                 .name(userDto.getName())
