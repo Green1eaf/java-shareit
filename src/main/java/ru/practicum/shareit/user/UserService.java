ec74acda167f2f6ce88c1b65a1f19cf1b8d74542
aleckid77@gmail.com <MyGitHub1>
2023-07-21 15:59:10 +0300

    feat: user repo and service

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -2,10 +2,10 @@ package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.AlreadyExistsException;
 import ru.practicum.shareit.exception.NotExistException;
 import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.dto.UserStorage;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,19 +15,17 @@ import static ru.practicum.shareit.user.UserMapper.*;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
 
-    private final UserStorage storage;
+    private final UserRepository repository;
 
     public UserDto create(UserDto userDto) {
-        if (findByEmail(userDto.getEmail()).isPresent()) {
-            throw new AlreadyExistsException("User with email:" + userDto.getEmail() + " already exists");
-        }
-        return toUserDto(storage.create(toUser(userDto)));
+        return toUserDto(repository.save(toUser(userDto)));
     }
 
     public UserDto findById(long id) {
-        return Optional.ofNullable(storage.findById(id))
+        return repository.findById(id)
                 .map(UserMapper::toUserDto)
                 .orElseThrow(() -> new NotExistException("User with id=" + id + " not exists"));
     }
@@ -41,23 +39,22 @@ public class UserService {
             checkForDuplicateEmail(userDto.getEmail(), userId);
             updatedUser.setEmail(userDto.getEmail());
         }
-        return toUserDto(storage.update(toUser(updatedUser)));
+        return toUserDto(repository.save(toUser(updatedUser)));
     }
 
     public void deleteById(long id) {
-        storage.deleteById(id);
+        repository.deleteById(id);
     }
 
     public List<UserDto> findAll() {
-        return storage.findAll().stream()
+        return repository.findAll().stream()
                 .map(UserMapper::toUserDto)
                 .collect(Collectors.toList());
     }
 
     public Optional<UserDto> findByEmail(String email) {
-        return findAll().stream()
-                .filter(user -> email.equals(user.getEmail()))
-                .findFirst();
+        return repository.findByEmail(email)
+                .map(UserMapper::toUserDto);
     }
 
     private void checkForDuplicateEmail(String email, long userId) {
